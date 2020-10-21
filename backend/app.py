7aed13d89a8c7312c60579e51bc7be394996089d
Asseme n'guessan <nguessanasseme@gmail.com>
2020-10-21 17:08:37 +0200

    Visualisation done

--- backend/app.py
+++ backend/app.py
@@ -11,10 +11,14 @@ jsons = requests.get("https://data.culture.gouv.fr/api/records/1.0/search/?datas
 dataset = pd.DataFrame.from_dict(jsons,orient="index")
 data=dataset[0][2]
 
+#bar_chart.x_labels = map(str, range(1980, 1985))
+
+criteres = ["entrees_millions","recette_guichet_meu_courants","recette_moyenne_par_entree_eu","seances_milliers"]
+types = ["bar","line"]
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',data=data,fields=criteres,types=types)
 
 @app.route('/seances')
 def seance():
@@ -42,6 +46,33 @@ def details(annee):
             data = d
             
     return render_template('details.html',datas=data)
+
+
+@app.route('/viz_seances_milliers')
+def viz_seances_milliers():
+    if(request.args.get('type') == "bar"):
+        bar_chart = pygal.Bar()
+        bar_chart.title = request.args.get('critere')
+        l=list()
+        for j in range(len(data)):
+            if(data[j]['fields']['annee']==request.args.get('start')):
+                for i in range(j,j+int(request.args.get('step'))+1):
+                    l.append(data[i]['fields'][request.args.get('critere')])
+        bar_chart.x_labels = map(str, range(int(request.args.get('start')), int(request.args.get('step'))+int(request.args.get('start'))+1))
+        bar_chart.add(request.args.get('critere'),  l)
+        bar=bar_chart.render_data_uri()
+    else:
+        line_chart = pygal.Line()
+        line_chart.title = request.args.get('critere')
+        l=list()
+        for j in range(len(data)):
+            if(data[j]['fields']['annee']==request.args.get('start')):
+                for i in range(j,j+int(request.args.get('step'))+1):
+                    l.append(data[i]['fields'][request.args.get('critere')])
+        line_chart.x_labels = map(str, range(int(request.args.get('start')), int(request.args.get('step'))+int(request.args.get('start'))+1))
+        line_chart.add(request.args.get('critere'),  l)
+        bar=line_chart.render_data_uri()
+    return render_template('index.html',data=data,bar=bar,fields=criteres,types=types)
 
 if __name__ == '__main__':
     app.run( debug = True )
